326c3a55ea284dcd573176b73187c415c90eeb7e
DEVfancybear <duonghacker2345@gmail.com>
2020-03-24 19:35:04 +0700

    fix component Shop

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -4,35 +4,6 @@ import ShopSidebar from "../components/ShopSideBar";
 import ShopFilter from "../components/ShopFilter";
 
 const Shop = () => {
-  <div className="col-sm-12 col-md-6 col-lg-4 p-b-50" key={index}>
-    <div className="block2">
-      <div className="block2-img wrap-pic-w of-hidden pos-relative">
-        {/* <img src={product.thumbnail} alt={product.name} /> */}
-
-        <div className="block2-overlay trans-0-4">
-          <a href="#" className="block2-btn-addwishlist hov-pointer trans-0-4">
-            <i className="icon-wishlist icon_heart_alt" aria-hidden="true" />
-            <i
-              className="icon-wishlist icon_heart dis-none"
-              aria-hidden="true"
-            />
-          </a>
-          <div className="block2-btn-addcart w-size1 trans-0-4">
-            <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
-              Add to Cart
-            </button>
-          </div>
-        </div>
-      </div>
-
-      <div className="block2-txt p-t-20">
-        <a className="block2-name dis-block s-text3 p-b-5">{product.name}</a>
-
-        {/* <span className='block2-price m-text6 p-r-5'>${product.price}</span> */}
-      </div>
-    </div>
-  </div>;
-
   return (
     <main>
       <section className="bgwhite p-t-55 p-b-65">
@@ -46,7 +17,41 @@ const Shop = () => {
               {/* <ShopFilter totalItems={totalItems} />
 
                 <div className='row'>{productList}</div> */}
+              <div className="col-sm-12 col-md-6 col-lg-4 p-b-50">
+                <div className="block2">
+                  <div className="block2-img wrap-pic-w of-hidden pos-relative">
+                    {/* <img src={product.thumbnail} alt={product.name} /> */}
 
+                    <div className="block2-overlay trans-0-4">
+                      <a
+                        href="#"
+                        className="block2-btn-addwishlist hov-pointer trans-0-4"
+                      >
+                        <i
+                          className="icon-wishlist icon_heart_alt"
+                          aria-hidden="true"
+                        />
+                        <i
+                          className="icon-wishlist icon_heart dis-none"
+                          aria-hidden="true"
+                        />
+                      </a>
+                      <div className="block2-btn-addcart w-size1 trans-0-4">
+                        <button className="flex-c-m size1 bg4 bo-rad-23 hov1 s-text1 trans-0-4">
+                          Add to Cart
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="block2-txt p-t-20">
+                    {/* <a className="block2-name dis-block s-text3 p-b-5">{product.name}</a> */}
+
+                    {/* <span className='block2-price m-text6 p-r-5'>${product.price}</span> */}
+                  </div>
+                </div>
+              </div>
+              ;
               {/* <Pagination
                   currentPage={currentPage}
                   totalItems={totalItems}

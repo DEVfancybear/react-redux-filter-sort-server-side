a9d62f9e43f9a548ea1b01c025558bb7a4c742bf
DEVfancybear <duonghacker2345@gmail.com>
2020-03-24 20:55:26 +0700

    fisnish

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,12 +4,11 @@ const URL_API = "https://next-shop-rest-api.herokuapp.com/products";
 export const fetchDataToServer = (sort, order) => {
   return async dispatch => {
     const resp = await axios.get(`${URL_API}?_sort=${sort}&_order=${order}`);
+    console.log(`${URL_API}?_sort=${sort}&_order=${order}`);
     const data = await resp.data;
     dispatch({
       type: types.FETCH_DATA_TO_SERVER,
-      payload: data,
-      sort,
-      order
+      payload: data
     });
   };
 };
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,17 +1,14 @@
 import * as types from "../constants/ActionTypes";
 const initialState = {
-  products: [],
-  sort: "id",
-  order: "desc"
+  products: []
 };
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.FETCH_DATA_TO_SERVER:
+      // console.log(action.payload)
       return {
         ...state,
-        products: action.payload,
-        sort: action.sort,
-        order: action.order
+        products: action.payload
       };
     default:
       return state;
